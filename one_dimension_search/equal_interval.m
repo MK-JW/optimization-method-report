68dd3b445cd039ad7eedae8b90796a08b0b13aa0
MK-JW <minjinwuwel1@163.com>
2024-12-22 11:06:20 +0800

    修改了对分搜索方法

--- one_dimension_search/equal_interval.m
+++ one_dimension_search/equal_interval.m
@@ -2,10 +2,12 @@ clc;
 clear;
 format short
 %%  给定一些初始参数
-a_low = -10; a_up = 10^3;
-tol = 10^-3;
+a_low = 0; a_up = 1;
+tol = 10^-4;
 k_count = 0;
-f = @(x)(1-x)^2;
+x_current = -0.5;
+d_current = 1;
+f = @(x) 2*x^2 - x -1;
 %%  进行迭代计算
 while(a_up - a_low>tol)
     a_interval = (a_up - a_low)/4;
@@ -13,7 +15,12 @@ while(a_up - a_low>tol)
     a2 = a_low + 2*a_interval;
     a3 = a_low + 3*a_interval;
     a_point = linspace(a_low,a_up,5);
-    f_value = [f(a_low),f(a1),f(a2),f(a3),f(a_up)];
+    x_alow = x_current + a_low*d_current;
+    x_a1 = x_current + a1*d_current;
+    x_a2 = x_current + a2*d_current;
+    x_a3 = x_current + a3*d_current;
+    x_aup = x_current + a_up*d_current;
+    f_value = [f(x_alow),f(x_a1),f(x_a2),f(x_a3),f(x_aup)];
     [~,I] = min(f_value);
     if(I == 1 )
         a_up = a_point(I+1);
@@ -26,4 +33,4 @@ while(a_up - a_low>tol)
     k_count = k_count + 1;
 end
 %%  得到结果
-a = (a_low + a_up)/2;
+alpha_star = (a_low + a_up)/2;
